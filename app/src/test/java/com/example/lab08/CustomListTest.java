45da287b2241ffa149fc4a0965558b9fbeb60449
Jiawang <jiawang3@ualberta.ca>
2026-03-06 12:53:04 -0700

    Failed countCity test

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,12 @@ public class CustomListTest {
         assertTrue(list.hasCity(calgary));
     }
 
+    @Test
+    public void testCountCity(){
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        assertEquals(0, list.countCities());
+    }
+
 }
